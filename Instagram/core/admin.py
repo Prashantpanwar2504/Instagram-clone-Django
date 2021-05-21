49b67b9449cfdd73f198b00cdea7755100b5c70a
prashantpanwar2504 <prashantpanwar423361@gmail.com>
2021-05-22 00:18:37 +0530

    creating post by logged in user

--- Instagram/core/admin.py
+++ Instagram/core/admin.py
@@ -6,8 +6,12 @@ from core.models import  (
     Follow,
 )
 # Register your models here.
+class PostModelAdmin(admin.ModelAdmin):
+    model = Post  # model we want to use
+    list_display = ('text', 'image', 'user', 'posted_on', 'updated_on')
 
-admin.site.register(Post)
+
+admin.site.register(Post, PostModelAdmin)
 admin.site.register(Comment)
 admin.site.register(Like)
 admin.site.register(Follow)
--- Instagram/core/models.py
+++ Instagram/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from crum import  get_current_user
 
 # Create your models here.
 
@@ -10,14 +11,23 @@ User = get_user_model()
 # first model => Post model
 class Post(models.Model):
     # id = models.AutoField(primary_key = True) django auto generate this for every model
-    text = models.TextField(max_length=140, blank=True, null=True) # null = None / blank = ''
+    text = models.TextField(max_length=140, blank=True, null=True)  # null = None / blank = ''
     image = models.ImageField(upload_to='post_images')  # Base_dir -> media -> post_images
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
-    posted_on = models.DateTimeField(auto_now_add=True) # auto_now_add = True is for, when post is created then it will auto fill the time
+    user = models.ForeignKey(User, on_delete=models.PROTECT, editable=False)
+    posted_on = models.DateTimeField(auto_now_add=True)  # auto_now_add = True is for, when post is created then it will auto fill the time
     updated_on = models.DateTimeField(auto_now=True)
 
-    def __str__(self): # String representation
+    def __str__(self):  # String representation
         return str(self.pk)
+
+    def save(self, *args, **kwargs):
+        # saved current logged in user
+        user = get_current_user()  # getting the current logged in user.
+        if user and not user.pk:  # is their any user and don't have the primary key of the user then user ko None krdo.
+            user = None           # agr if condition nhi chalegi iska matlab current user be h and primary key be h.
+        if not self.pk:  # if Post model ke user field ki primary key nhi h to, post model ke user ko current user  assign krdo.
+            self.user = user
+        super(Post, self).save(*args, **kwargs)  # getting the save method of parent class with the help of super().
 
 
 # second model => post_comments model
