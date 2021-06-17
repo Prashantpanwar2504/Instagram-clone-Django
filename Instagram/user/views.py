5b3f57e3cb8312b3bf1b91f89742200216468cff
prashantpanwar2504 <prashantpanwar423361@gmail.com>
2021-06-17 19:01:56 +0530

    Profile is avaiable now

--- Instagram/user/views.py
+++ Instagram/user/views.py
@@ -5,7 +5,7 @@ from django.views.generic import View
 # Create your views here.
 
 class ProfileView(View):
-    template_name_anon = 'user/anonymous_profile.html'
+    template_name_anom = 'user/anonymous_profile.html'
     template_name_auth = 'user/authenticated_profile.html'
 
     def get(self, request, *args, **kwargs):
